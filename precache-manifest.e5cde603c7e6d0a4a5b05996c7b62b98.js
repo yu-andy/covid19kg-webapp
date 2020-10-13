17a6fb99fefca19c6f76a7134ac23d32f7dfe219
andy <andy@andys-MacBook-Pro.local>
2020-10-13 14:24:04 +1030

    Updates

---
+++ precache-manifest.e5cde603c7e6d0a4a5b05996c7b62b98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aefb737f7de40fdc00f97d4eaea5c1e8",
+    "url": "/covid19kg-webapp/index.html"
+  },
+  {
+    "revision": "773ded0c5fd2ed020773",
+    "url": "/covid19kg-webapp/static/css/main.cfec6834.chunk.css"
+  },
+  {
+    "revision": "c53c9a34cd9dea64a7ff",
+    "url": "/covid19kg-webapp/static/js/2.41348316.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19kg-webapp/static/js/2.41348316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "773ded0c5fd2ed020773",
+    "url": "/covid19kg-webapp/static/js/main.cf2ab2a3.chunk.js"
+  },
+  {
+    "revision": "f9ba24d07576d0bf7a10",
+    "url": "/covid19kg-webapp/static/js/runtime-main.e2e80758.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19kg-webapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
